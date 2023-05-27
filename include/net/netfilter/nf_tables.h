6a61fa3f1f2b9a2796f3b1bc893c34e5b6b79036
Pablo Neira Ayuso <pablo@netfilter.org>
2023-05-27 18:08:03 +0200

    netfilter: nftables: statify nft_parse_register()
    [ 08a01c11a5bb3de9b0a9c9b2685867e50eda9910 ]
    
    This function is not used anymore by any extension, statify it.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -177,7 +177,6 @@ static inline enum nft_registers nft_type_to_reg(enum nft_data_types type)
 }
 
 int nft_parse_u32_check(const struct nlattr *attr, int max, u32 *dest);
-unsigned int nft_parse_register(const struct nlattr *attr);
 int nft_dump_register(struct sk_buff *skb, unsigned int attr, unsigned int reg);
 
 int nft_validate_register_load(enum nft_registers reg, unsigned int len);
