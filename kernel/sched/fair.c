d8580f2747822095c33e7e391bd5ee0d105c0e47
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-11-11 15:00:12 +0800

    sched/bore: enable sched_burst_smoothness_short by default

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -784,7 +784,7 @@ static u64 sched_vslice(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #ifdef CONFIG_SCHED_BORE
 uint __read_mostly sched_bore                   = 1;
 uint __read_mostly sched_burst_smoothness_long  = 1;
-uint __read_mostly sched_burst_smoothness_short = 0;
+uint __read_mostly sched_burst_smoothness_short = 1;
 uint __read_mostly sched_burst_fork_atavistic   = 2;
 uint __read_mostly sched_burst_penalty_offset   = 22;
 uint __read_mostly sched_burst_penalty_scale    = 1280;
